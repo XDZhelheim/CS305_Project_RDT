433b1d66ffa090e6ecbdf525a07559306f66f917
XDZhelheim <601251915@qq.com>
2020-12-14 22:34:31 +0800

    segment

--- rdt.py
+++ rdt.py
@@ -1,23 +1,146 @@
-from udp import UDPsocket
+from USocket import UnreliableSocket
+import threading
+import time
 
-class socket(UDPsocket):
-    def __init__(self):
-        super(socket, self).__init__()
+class RDTSocket(UnreliableSocket):
+    """
+    The functions with which you are to build your RDT.
+    -   recvfrom(bufsize)->bytes, addr
+    -   sendto(bytes, address)
+    -   bind(address)
 
-    # send syn; receive syn, ack; send ack
-    def connect(self):
-        pass
+    You can set the mode of the socket.
+    -   settimeout(timeout)
+    -   setblocking(flag)
+    By default, a socket is created in the blocking mode. 
+    https://docs.python.org/3/library/socket.html#socket-timeouts
 
-    # receive syn; send syn, ack; receive ack
-    def accept(self):
-        pass
+    """
+    def __init__(self, rate=None, debug=True):
+        super().__init__(rate=rate)
+        self._rate = rate
+        self._send_to = None
+        self._recv_from = None
+        self.debug = debug
+        #############################################################################
+        # TODO: ADD YOUR NECESSARY ATTRIBUTES HERE
+        #############################################################################
+        
+        #############################################################################
+        #                             END OF YOUR CODE                              #
+        #############################################################################
 
-    # send fin; recieve ack; recieve fin; send ack
+    def accept(self)->(RDTSocket, (str, int)):
+        """
+        Accept a connection. The socket must be bound to an address and listening for 
+        connections. The return value is a pair (conn, address) where conn is a new 
+        socket object usable to send and receive data on the connection, and address 
+        is the address bound to the socket on the other end of the connection.
+
+        This function should be blocking. 
+        """
+        conn, addr = RDTSocket(self._rate), None
+        #############################################################################
+        # TODO: YOUR CODE HERE                                                      #
+        #############################################################################
+       
+        #############################################################################
+        #                             END OF YOUR CODE                              #
+        #############################################################################
+        return conn, addr
+
+    def connect(self, address:(str, int)):
+        """
+        Connect to a remote socket at address.
+        Corresponds to the process of establishing a connection on the client side.
+        """
+        #############################################################################
+        # TODO: YOUR CODE HERE                                                      #
+        #############################################################################
+        raise NotImplementedError()
+        #############################################################################
+        #                             END OF YOUR CODE                              #
+        #############################################################################
+
+    def recv(self, bufsize:int)->bytes:  # bufsize参数是bytes
+        """
+        Receive data from the socket. 
+        The return value is a bytes object representing the data received. 
+        The maximum amount of data to be received at once is specified by bufsize. 
+        
+        Note that ONLY data send by the peer should be accepted.
+        In other words, if someone else sends data to you from another address,
+        it MUST NOT affect the data returned by this function.
+        """
+        data = None
+        assert self._recv_from, "Connection not established yet. Use recvfrom instead."
+        #############################################################################
+        # TODO: YOUR CODE HERE                                                      #
+        #############################################################################
+        
+        #############################################################################
+        #                             END OF YOUR CODE                              #
+        #############################################################################
+        return data
+
+    def send(self, bytes:bytes):
+        """
+        Send data to the socket. 
+        The socket must be connected to a remote socket, i.e. self._send_to must not be none.
+        """
+        assert self._send_to, "Connection not established yet. Use sendto instead."
+        #############################################################################
+        # TODO: YOUR CODE HERE                                                      #
+        #############################################################################
+        raise NotImplementedError()
+        #############################################################################
+        #                             END OF YOUR CODE                              #
+        #############################################################################
+
     def close(self):
-        pass
-
-    def recv(self):
-        pass
+        """
+        Finish the connection and release resources. For simplicity, assume that
+        after a socket is closed, neither futher sends nor receives are allowed.
+        """
+        #############################################################################
+        # TODO: YOUR CODE HERE                                                      #
+        #############################################################################
+        
+        #############################################################################
+        #                             END OF YOUR CODE                              #
+        #############################################################################
+        super().close()
+        
+    def set_send_to(self, send_to):
+        self._send_to = send_to
     
-    def send(self):
-        pass
+    def set_recv_from(self, recv_from):
+        self._recv_from = recv_from
+
+class Segment:
+    '''
+    field       length          range               type
+    ----------------------------------------------------
+    syn:        1 bit           0 ~ 1               bool
+    fin:        1 bit           0 ~ 1               bool
+    ack:        1 bit           0 ~ 1               bool
+    seq_num:    4 byte=32 bit   0 ~ 4294967295      int
+    ack_num:    4 byte=32 bit   0 ~ 4294967295      int
+    length:     4 byte=32 bit   0 ~ 4294967295      int
+    checksum:   2 byte=16 bit   0 ~ 65535           int
+    payload:    length byte     0 ~ 2^34359738360   bytes
+    '''
+
+    MAX_NUM=4294967295 # 2^32-1
+
+    def __init__(self, syn, fin, ack, seq_num, ack_num, length, payload):
+        self.syn=syn
+        self.fin=fin
+        self.ack=ack
+        self.seq_num=seq_num%Segment.MAX_NUM
+        self.ack_num=ack_num%Segment.MAX_NUM
+        self.length=length
+        self.payload=payload
+
+    
+
