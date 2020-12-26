ffe49823c861f7491a47649d1d93228d2e5dfd48
XDZhelheim <601251915@qq.com>
2020-12-26 11:32:33 +0800

    错包测试

--- network.py
+++ network.py
@@ -2,7 +2,7 @@ from socket import socket, AF_INET, SOCK_DGRAM, inet_aton, inet_ntoa
 import random, time
 import threading, queue
 from socketserver import ThreadingUDPServer 
-# test
+
 lock = threading.Lock()
 
 def bytes_to_addr(bytes):
@@ -11,39 +11,62 @@ def bytes_to_addr(bytes):
 def addr_to_bytes(addr):
     return inet_aton(addr[0]) + addr[1].to_bytes(4, 'big')
 
-def corrupt(data: bytes) -> bytes:
-    raw = list(data)
-    for _ in range(0, random.randint(0, 3)):
-        pos = random.randint(0, len(raw) - 1)
-        raw[pos] = random.randint(0, 255)
-    return bytes(raw)
-
 class Server(ThreadingUDPServer):
-    def __init__(self, addr, rate=None, delay=None, corrupt=None):
+    def __init__(self, addr, rate=None, delay=None):
         super().__init__(addr, None)
-        self.rate = rate
+        self.rate = rate            
         self.buffer = 0
         self.delay = delay
-        self.corrupt = corrupt
 
     def verify_request(self, request, client_address):
-        if self.buffer<10:
-            self.buffer+=1
+        """
+        request is a tuple (data, socket)
+        data is the received bytes object
+        socket is new socket created automatically to handle the request
+
+        if this function returns False， the request will not be processed, i.e. is discarded.
+        details: https://docs.python.org/3/library/socketserver.html
+        """
+        if self.buffer < 100000: # some finite buffer size (in bytes)
+            self.buffer += len(request[0])
             return True
         else:
             return False
         
     def finish_request(self, request, client_address):
         data, socket = request
-        lock.acquire()
-        if self.rate: time.sleep(len(data)/self.rate)
-        self.buffer -= 1
-        lock.release()
+        loss_rate=0
+        corrupt_rate=0.1
         
-        to = bytes_to_addr(data[:8])
-        # print(client_address, to)
-        socket.sendto(addr_to_bytes(client_address) + data[8:], to)
+        with lock:
+            if self.rate: time.sleep(len(data)/self.rate)
+            self.buffer -= len(data)
+            if random.random() < loss_rate:
+                return
 
+            select_num=10
+            if random.random() < corrupt_rate:
+                data=bytearray(data)
+                for _ in range(select_num):
+                    index=random.randint(8, len(data)-1)
+                    temp=data.copy()
+                    data=data[:index]
+                    data.extend(random.randint(0, 255).to_bytes(1, 'big'))
+                    data.extend(temp[index+1:])
+                data=bytes(data)
+            
+        """ 
+        this part is not blocking and is executed by multiple threads in parallel
+        you can add random delay here, this would change the order of arrival at the receiver side.
+        
+        for example:
+        time.sleep(random.random())
+        """
+
+        to = bytes_to_addr(data[:8])
+        print(client_address, to) # observe tht traffic
+        socket.sendto(addr_to_bytes(client_address) + data[8:], to)
+    
 server_address = ('127.0.0.1', 12345)
 
 if __name__=='__main__':
